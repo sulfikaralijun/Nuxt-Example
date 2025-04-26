b65b2b4a889b80dd7979c6c7ef14f191dd556564
sulfikaralijun <sulfikaralijun@gmail.com>
2025-04-26 18:57:04 +0800

    tes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto p-6">
     <h1 class="text-3xl font-bold mb-4">Welcome to My Nuxt App</h1>
-    <p class="mb-4">This is the homepage of simple Nuxt application.</p>
+    <p class="mb-4">This is the homepage of our simple Nuxt application.</p>
   </div>
 </template>

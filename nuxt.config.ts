cae4a604b320768590669c11dfe11c6a634bca3c
sulfikaralijun <sulfikaralijun@gmail.com>
2025-05-03 17:09:35 +0800

    update for pm2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: "2024-11-01",
-    devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss"],
-    app: {
-        head: {
-            title: "Simple Nuxt App",
-            meta: [
-                { charset: "utf-8" },
-                {
-                    name: "viewport",
-                    content: "width=device-width, initial-scale=1",
-                },
-            ],
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Simple Nuxt App",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
         },
+      ],
     },
+  },
 });
